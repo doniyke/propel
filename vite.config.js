9b4b50016e81c985de8b44cb0d43e1eae1d46fea
Davis <iykedavis69@gmail.com>
2023-11-06 03:07:56 +0100

    Update base url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     base: 'https://propel-test-4a1b0257790a.herokuapp.com/',
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['https://propel-test-4a1b0257790a.herokuapp.com/resources/css/app.css', 'https://propel-test-4a1b0257790a.herokuapp.com/resources/js/app.js'],
             refresh: true,
         }),
         vue(),
